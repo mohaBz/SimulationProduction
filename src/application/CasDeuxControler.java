b7ed058c5a208a2ff19503d044b7e12b1a0d0102
bzmoha <bzmoha99@gmail.com>
2020-08-11 15:48:51 +0200

    Revert "Revert "submit observations to firestore""
    This reverts commit 16d8e0a3ed491e3c961648bfba0af7d4fd1b6be3.

--- src/application/CasDeuxControler.java
+++ src/application/CasDeuxControler.java
@@ -1,10 +1,99 @@
 package application;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class CasDeuxControler {
     @FXML
     private TextField quantCommande;
+    @FXML
+    private TextField numCamion;
+    @FXML
+    private TextField capCamion;
+    @FXML
+    private TextField numDepot;
+    @FXML
+    private TextField capDepot;
+    @FXML
+    private Button startSimulationBt;
+    @FXML
+    private Label errorLb;
 
+    @FXML
+    public void initialize(){
+        startSimulationBt.setDisable(true);
+        // force the field to be numeric only
+        initTextFields(quantCommande);
+        initTextFields(numCamion);
+        initTextFields(capCamion);
+        initTextFields(numDepot);
+        initTextFields(capDepot);
+    }
+
+
+
+
+    public void startSimulation(){
+        DataHolder dataHolder=DataHolder.getInstance();
+        int numProduit=Integer.parseInt(quantCommande.getText());
+        int quantMat=(int)(numProduit
+                /(0.9*0.93*0.95));
+        System.out.printf(quantMat+"matrial premiere");
+        dataHolder.setMatQuantity(quantMat);
+        dataHolder.setNumCamion(Integer.parseInt(numCamion.getText()));
+        dataHolder.setCapCamion(Integer.parseInt(capCamion.getText()));
+        dataHolder.setNumDepot(Integer.parseInt(numDepot.getText()));
+        dataHolder.setCapDepot(Integer.parseInt(capDepot.getText()));
+        int numDepots=numProduit/dataHolder.getCapDepot()+1;
+        int numCamions=numProduit/dataHolder.getCapCamion()+1;
+        if(numCamions>dataHolder.getNumCamion())
+        {
+            System.out.printf("not enough camion");
+            errorLb.setText("camions ne suffisent pas");
+            return;
+        }
+        if(numDepots>dataHolder.getNumDepot())
+        {
+            System.out.printf("not enough depot");
+            errorLb.setText("depots ne suffisent pas");
+            return;
+        }
+        try
+        {
+            Stage stage=Main.getStage();
+            Parent root= FXMLLoader.load(getClass().getResource("ResultSimulation.fxml"));
+            Scene scene = new Scene(root,600,400);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+
+        }
+    }
+    private boolean verifyText(TextField text){
+        return text.getText().isEmpty() || text.getText().trim().isEmpty();
+    }
+
+    private void initTextFields(TextField textField){
+        textField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue,
+                                String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    textField.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+                if (verifyText(quantCommande)||verifyText(numCamion)||verifyText(capCamion)||verifyText(numDepot)||verifyText(capDepot))
+                    startSimulationBt.setDisable(true);
+                else
+                    startSimulationBt.setDisable(false);
+            }
+        });
+    }
 }
